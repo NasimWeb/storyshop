336831edf6e6d6bf02ad2ac2108bc2fb41301c71
Nasim <nasim.web.develop@gmail.com>
2024-03-31 12:56:17 -0700

    fix changes

--- src/Components/SaleBoxs/SaleBoxs.jsx
+++ src/Components/SaleBoxs/SaleBoxs.jsx
@@ -6,7 +6,7 @@ import './SaleBoxs.css'
 export default function SaleBoxs({  children}) {
 
   return (
-    <div className="sales-box w-1/2 text-center  lg:w-1/4 bg-zinc-900 flex flex-col rounded-lg p-4">
+    <div className="sales-box w-full text-center  lg:w-1/4 bg-zinc-900 flex flex-col rounded-lg p-4">
       {children}
     </div>
   );
